957208b71b6e1d6830105185c7347a028a1a97b6
Anonyzard <anonyzard@disroot.org>
2022-10-27 15:36:25 -0300

    Updating json and add.py

--- add.py
+++ add.py
@@ -15,7 +15,7 @@ description = input("\nEscribe una corta descripción del programa: ")
 web = input("\nEscribe la URL de la web del programa: ")
 mantainer = input("\nEscribe el nombre del mantenedor del paquete: ")
 license = input("\nEscribe el nombre de la licencia del programa: ")
-datos[name] = {
+nuevo = {
         "name": name,
         "package": package,
         "version": version,
@@ -27,8 +27,15 @@ datos[name] = {
         "license": license,
         "remote": True
     }
-print(datos[name])
+# datos["lpkg"].append = nuevo
+# json.dump(datos, archivo_nuevo, indent=4, ensure_ascii=False)
+#print(datos["lpkg"])
 archivo.close()
+# data = dict(datos)
+# data["lpkg"].append = nuevo
 with open(salida, 'w') as archivo_nuevo:
-    json.dump(datos, archivo_nuevo, indent=4)
+    datos["lpkg"].append(nuevo)
+    datos["lpkg"] = sorted(datos["lpkg"], key=lambda x: x["name"])
+    #print(datos)
+    json.dump(datos, archivo_nuevo, indent=4, ensure_ascii=False)
 archivo_nuevo.close()
